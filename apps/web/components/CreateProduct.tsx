3c69fd7501b2d9747474efd6ca451cbfc98c4245
Guilherme <gui.mochila@gmail.com>
2022-02-21 07:24:04 +0100

    feature: Add styled Form

--- apps/web/components/CreateProduct.tsx
+++ apps/web/components/CreateProduct.tsx
@@ -1,48 +1,76 @@
+import { FormEvent } from 'react'
 import useForm from '../hooks/useForm'
+import Form from './styled/Form'
 
 interface IFormState {
   name: string
   price: number
+  image: string
+  description: string
 }
 
 function CreateProduct() {
-  const { inputs, handleChange, clearForm, resetForm } = useForm<IFormState>({
+  const { inputs, handleChange } = useForm<IFormState>({
+    image: '',
     name: '',
     price: 0,
+    description: '',
   })
 
-  return (
-    <form>
-      <label htmlFor="name">
-        Name
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Name"
-          value={inputs.name}
-          onChange={handleChange}
-        />
-      </label>
-      <label htmlFor="price">
-        Price
-        <input
-          type="number"
-          id="price"
-          name="price"
-          placeholder="Price"
-          value={inputs.price}
-          onChange={handleChange}
-        />
-      </label>
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    console.log(inputs)
+  }
 
-      <button type="button" onClick={clearForm}>
-        Clear Form
-      </button>
-      <button type="button" onClick={resetForm}>
-        Reset Form
-      </button>
-    </form>
+  return (
+    <Form onSubmit={handleSubmit}>
+      <fieldset>
+        <label htmlFor="image">
+          Image
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="Price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          ></textarea>
+        </label>
+
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
   )
 }
 
--- apps/web/hooks/useForm.ts
+++ apps/web/hooks/useForm.ts
@@ -3,7 +3,9 @@ import { ChangeEvent, useState } from 'react'
 function useForm<T>(initialState: T | { [k: string]: any }) {
   const [inputs, setInputs] = useState(initialState)
 
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+  function handleChange(
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) {
     const inputKey = e.target.name || e.target.id
     let inputValue: any = e.target.value
 
@@ -11,6 +13,10 @@ function useForm<T>(initialState: T | { [k: string]: any }) {
       inputValue = inputValue !== '' ? parseInt(inputValue) : ''
     }
 
+    if (e.target.type === 'file') {
+      const target = e.target as HTMLInputElement
+      inputValue = target.files?.length ? target.files[0] : ''
+    }
     setInputs({
       ...inputs,
       [inputKey]: inputValue,
